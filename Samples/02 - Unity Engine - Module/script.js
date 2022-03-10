67c93940a4a15c166e1bcc79827e285ab4577339
Sébastien BIAUDET <sebastien.biaudet@gmail.com>
2022-03-10 18:00:08 +0100

    fix licence key

--- Samples/02 - Unity Engine - Module/script.js
+++ Samples/02 - Unity Engine - Module/script.js
@@ -1,8 +1,6 @@
 import SpiritEngine from "https://cdn.retorik.ai/spiritengine/sdk/alpha/spiritengine.js";
-//const SpiritEngine = require('retorik-spiritengine')
 
 var licence = 'your-licence-here';
-licence='K0vi08nY6JDn68ZF5ByDt8THzbxVoGFSJEJ67b5UsCJDchQ01V2J2RuiNr8iDzxGyT1Ablcz1hn5sRAQuR8UUJ!lFpRlx2goKdpLc+LJ8XDby+H2kMeCbaj!7SI6ZpCVo!89SrDuGxAOJZZcg1JlvX3X9byqD8OhqWPSRgjuNVFj+IU2G3dbe75sE5M2DCGizI5FbnsgpYdcjghZbacJE6kuxP2cyDMDrJif9R6Qn7DTFItzYHkeaZQJq70JWlmeMLu1Er51iyc5OwdmCNWEwW2pDC7htQmRDh27ZQ4Mw03VqOprlh8yIq361aU3ZTHRUcO9XuuWEbLaUO47fjmCZw=='
 const animationLocation = "https://cdn.retorik.ai/spiritengine/Jean-Pierre/";
 
 window.spiritEngine = new SpiritEngine(licence, animationLocation);
